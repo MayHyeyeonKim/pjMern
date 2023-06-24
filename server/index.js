d235be01fd8999822419d78cda3e31b2de7ace21
hyeyeonkim <hkim7963@gmail.com>
2023-06-23 22:57:26 -0700

    axios & cors

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 5001;
+const port = 8005;
+const mongoose = require("mongoose");
+const { connectionString } = require("./credentials");
 
 app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const { Post } = require("./Model/Post.js");
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  mongoose
+    .connect(connectionString, {
+      dbName: "classproject",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log(`Example app listening on port ${port}`);
+      console.log(`"localhost ${port}"를 브라우저에 찍어봐`);
+    })
+    .catch((err) => {
+      console.log(`${err}를 해결하시오!`);
+    });
 });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.post("/api/test", (req, res) => {
+  const communitypost = new Post({ title: "fluffy" });
+  res.status(200).json({ success: true, text: "안냥" });
 });
