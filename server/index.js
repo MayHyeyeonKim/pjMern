ebb4c363fe00c47a48be8a391d267a181818f4bd
hyeyeonkim <hkim7963@gmail.com>
2023-07-22 00:47:07 -0700

    feat: implement fetching post list and post

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const { Post } = require("./Model/Post.js");
 app.listen(port, () => {
   mongoose
     .connect(connectionString, {
-      dbName: "classproject",
+      dbName: "pj-mern-db",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
@@ -30,7 +30,28 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.post("/api/test", (req, res) => {
-  const communitypost = new Post({ title: "fluffy" });
-  res.status(200).json({ success: true, text: "안냥" });
+//클라이언트단에서 /api/test로 post 요청이 온다면은
+app.post("/api/post/submit", (req, res) => {
+  let temp = req.body;
+  // console.log(temp);
+  const communitypost = new Post(temp);
+  communitypost
+    .save()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false });
+    });
+});
+
+app.post("/api/post/list", (req, res) => {
+  Post.find()
+    .exec()
+    .then((doc) => {
+      res.status(200).json({ success: true, postList: doc });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false });
+    });
 });
